d01e532b3a11f83242e973867dab55a45261046a
Nguyễn Lê Anh Duy <nguyenleanhduy9412@gmail.com>
2026-01-14 13:12:50 +0700

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>
+        We are a technology company focused on building modern web applications
+        using React.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
 
