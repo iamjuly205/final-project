20284c44f2beb814de791fd6b85eb19336cd8c24
Nguyễn Lê Anh Duy <nguyenleanhduy9412@gmail.com>
2026-01-14 13:14:46 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div>
+        <h1>Paradise Nursery</h1>
+        <p>Your one-stop destination for beautiful plants</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
