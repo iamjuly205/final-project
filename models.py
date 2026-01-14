333b36729c8e9939fbdd0cb851e52e7f62de9632
Nguyễn Lê Anh Duy <nguyenleanhduy9412@gmail.com>
2026-01-14 21:23:10 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 class Question(models.Model):
-    ...
+    # các field khác...
+    grade = models.IntegerField(default=1)
 
-class Choice(models.Model):
-    ...
-
-class Submission(models.Model):
-    ...
+    def is_get_score(self, selected_ids):
+        correct_choices = self.choice_set.filter(is_correct=True)
+        correct_ids = set(correct_choices.values_list('id', flat=True))
+        return correct_ids.issubset(set(selected_ids))
