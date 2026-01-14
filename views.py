6a7181a730897cca03771b01e7e81bc44a60e3d4
Nguyễn Lê Anh Duy <nguyenleanhduy9412@gmail.com>
2026-01-14 21:23:44 +0700

    Update views.py

--- views.py
+++ views.py
@@ -29,8 +29,22 @@ def show_exam_result(request, course_id, submission_id):
     course = get_object_or_404(Course, pk=course_id)
     submission = get_object_or_404(Submission, pk=submission_id)
 
-    return render(request, 'onlinecourse/exam_result.html', {
+    selected_ids = submission.choices.values_list('id', flat=True)
+
+    total_score = 0
+    possible_score = 0
+
+    for question in course.question_set.all():
+        possible_score += question.grade
+        if question.is_get_score(selected_ids):
+            total_score += question.grade
+
+    context = {
         'course': course,
         'submission': submission,
-        'score': submission.score
-    })
+        'selected_ids': selected_ids,
+        'grade': total_score,
+        'possible': possible_score,
+    }
+
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
